345c8d9dfb0cd701c9ff78825b3ad53d8dc46328
eurydna <andrew.aknh@gmail.com>
2026-04-14 02:32:58 -0400

    still fixing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('leaflet') || id.includes('react-leaflet')) {
+              return 'map'; // Isolates map logic
+            }
+            if (id.includes('@dnd-kit')) {
+              return 'dnd'; // Isolates drag-and-drop
+            }
+            if (id.includes('react/') || id.includes('react-dom/') || id.includes('react-router-dom/')) {
+              return 'vendor'; // Isolates core React
+            }
+          }
+        }
+      }
+    }
+  }
 })
